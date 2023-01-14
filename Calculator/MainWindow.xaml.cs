9ebcde80b962b7fd0be5a57755f247ff0cf9aab3
LiamM <LiamM@LIAM>
2023-01-14 17:47:31 +0000

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber, result; // last number in the calculator is being held here.
+        SelectedOperator selectedOperator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalsButton.Click += EqualsButton_Click;
+
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))//assign the new number to be stored into a variable called new number
+
+            {
+                switch(selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.Subtraction(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+
+                }
+                resultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+
+        {
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
+            {
+                
+                tempNumber = tempNumber / 100;
+                if(lastNumber != 0)
+                {
+                    tempNumber *= lastNumber;
+                }
+                resultLabel.Content = tempNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+            result = 0;
+            lastNumber= 0;
+
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber)) //out lastNumber saves the last input number in to a variable (lastNumber) which can then be called upon again
+            {
+
+                resultLabel.Content = 0;
+            }
+
+            if (sender == multipleButton)
+                selectedOperator = SelectedOperator.Multiplication;
+            if(sender == divisionButton)
+                selectedOperator = SelectedOperator.Division;
+            if (sender == plusButton)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == minusButton)
+                selectedOperator = SelectedOperator.Subtraction;
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+                //do nothing
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+            
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
+            // rather than write 9 if statments what is happening here is the 'sender' is being used as a 'button' as its currently an object.
+            // By doing this it allows us to grab the 'content' of the button which is the number i.e 3.
+            // int parse converts it to an interger to allow us to use other functions later on i.e +, this will add the numbers and not concatenate 
+
+
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+    }
+
+            public enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Subtraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Divide(double n1, double n2)
+        {
+            if(n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong Operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return n1 / n2;
+
+            
+        }
+    }
+}
+    
+
